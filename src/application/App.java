b5b1596f2dcae9a998140b42ed59b7cfa62ef7dd
LauroHJ <laurohenchenjunior@gmail.com>
2023-05-31 09:21:14 -0400

    Começando a implementar Board e Piece

--- src/application/App.java
+++ src/application/App.java
@@ -1,19 +1,13 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class App {
     public static void main(String[] args) throws Exception {
      
-    Position pos = new Position(1, 2);
-
-    System.out.println(pos);
-    
-    
-    
-    
-    
-    
+        Board board = new Board(8, 8);
+     
     }
 
 
