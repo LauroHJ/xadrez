49998335d9032178c849c86f09ba7c4fdbc37f15
LauroHJ <laurohenchenjunior@gmail.com>
2023-05-31 08:54:07 -0400

    primeira classe: Position

--- src/application/App.java
+++ src/application/App.java
@@ -1,5 +1,20 @@
+package application;
+
+import boardgame.Position;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+     
+    Position pos = new Position(1, 2);
+
+    System.out.println(pos);
+    
+    
+    
+    
+    
+    
     }
+
+
 }
