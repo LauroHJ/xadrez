bbf6af84e2e74f1682cba898d2ddec27d7141406
LauroHJ <laurohenchenjunior@gmail.com>
2023-06-05 15:06:07 -0400

    Tratando exceções e limpando a tela

--- src/application/UI.java
+++ src/application/UI.java
@@ -8,7 +8,7 @@ import chess.ChessPosition;
 import chess.Color;
 
 public class UI {
-
+    
     public static final String ANSI_RESET = "\u001B[0m";
 	public static final String ANSI_BLACK = "\u001B[30m";
 	public static final String ANSI_RED = "\u001B[31m";
@@ -28,6 +28,10 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
     
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
     public static ChessPosition readChessPosition(Scanner sc){
         try{
             String s = sc.nextLine();
