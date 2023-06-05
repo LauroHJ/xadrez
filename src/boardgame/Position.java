2810541dff6d8049c17f3a1b5e190141c4de7d84
LauroHJ <laurohenchenjunior@gmail.com>
2023-06-05 17:26:19 -0400

    Implementando movimentos possíveis da Torre

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -22,10 +22,17 @@ public class Position {
     public void setColumn(int column) {
         this.column = column;
     }
+
+    public void setValues(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
     
     @Override
     public String toString() {
         return this.row + ", " + this.column;
     }
+
+
     
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -32,6 +32,7 @@ public class ChessMatch{
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
+        validateTragetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
         return (ChessPiece)capturedPiece;
     }
@@ -49,6 +50,12 @@ public class ChessMatch{
         }
         if (!board.piece(position).isThereAnyPossibleMove()){
             throw new ChessException("Não existe movimentos possíveis para a peça escolhida");
+        }
+    }
+
+    private void validateTragetPosition(Position source, Position target) {
+        if (!board.piece(source).possibleMove(target)) {
+            throw new ChessException("A peça escolhida não pode se mover para a posição escolhida");
         }
     }
     
